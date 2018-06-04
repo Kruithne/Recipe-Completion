46554594c137eca9a45998111b2862938bd63814
Kruithne <kruithne@gmail.com>
2018-06-04 16:31:00 +0100

    Highlight related label element when focusing on an .input-field

--- js/core.js
+++ js/core.js
@@ -16,6 +16,28 @@ $(function() {
 		});
 	});
 
+	// Dynamic highlighting and tab-indexing.
+	$('.input-field').each(function() {
+		var field = $(this);
+		var label = $('label[for=' + field.attr('id') + ']');
+		var next = $('#' + field.attr('data-tab'));
+
+		field.on('focus', function() {
+			label.addClass('selected');
+		}).on('blur', function() {
+			label.removeClass('selected');
+		}).on('keypress', function(e) {
+			if (e.which === 13) {
+				if (next.is('input[type=button]')) {
+					next.click();
+				} else {
+					next.focus();
+					setTimeout(function() { next.select(); }, 0);
+				}
+			}
+		});
+	});
+
 	// Wait for the background image to load before displaying.
 	loadImage('images/recipe-background.jpg', function(url) {
 		$('#background').css('background-image', 'url(' + url + ')').fadeIn(1000);
