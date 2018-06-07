42884f84c8271104d3275e3120b499e5d5c82387
Kruithne <kruithne@gmail.com>
2018-06-07 15:32:56 +0100

    Implement realm selector and flesh client functionality

--- js/core.js
+++ js/core.js
@@ -8,6 +8,136 @@ $(function() {
 		});
 	};
 
+	var api = function(req, callback) {
+		$.ajax({
+			url: 'endpoint.php',
+			type: 'POST',
+			contentType: 'application/json',
+			dataType: 'json',
+			async: true,
+			data: JSON.stringify(req),
+			success: callback
+		});
+	};
+
+	var statusElement = $('#status');
+	var statusText = statusElement.find('span').first();
+
+	var setStatus = function(text) {
+		statusText.text(text);
+		return statusElement.removeClass('pending error').show().css('display', 'flex');
+	};
+
+	var setPendingStatus = function(text) {
+		setStatus(text).addClass('pending');
+	};
+
+	var setErrorStatus = function(text) {
+		setStatus(text).addClass('error');
+	};
+
+	var hideStatus = function() {
+		statusElement.hide();
+	};
+
+	var selectedRealm = null;
+
+	var realmDropDown = $('#realm-drop');
+	var realmField = $('#field-realm');
+	var realmContainers = [];
+
+	var characterField = $('#field-character');
+
+	var selectOption = function(option) {
+		selectedRealm = { region: option.attr('data-region'), realm: option.attr('data-slug') };
+		realmField.val(option.text() + ' (' + selectedRealm.region.toUpperCase() + ')');
+	};
+
+	var hideRealmDropDown = function() {
+		realmDropDown.hide();
+		realmField.removeClass('activated');
+
+		var filter = realmField.val().trim().toLowerCase();
+		if (selectedRealm === null && filter.length > 0) {
+			$('.realm-option').each(function() {
+				var option = $(this);
+				if (option.attr('data-name').startsWith(filter)) {
+					selectOption(option);
+					return false;
+				}
+			});
+		}
+	};
+
+	var showRealmDropDown = function(filter) {
+		filter = filter.trim().toLowerCase();
+		selectedRealm = null;
+
+		if (filter.length > 0) {
+			realmDropDown.show();
+			realmField.addClass('activated');
+
+			for (var i = 0; i < realmContainers.length; i++) {
+				var realmContainer = realmContainers[i];
+				var displayCount = 0;
+
+				realmContainer.children('.realm-option').each(function() {
+					var option = $(this);
+
+					if (option.attr('data-name').startsWith(filter)) {
+						option.show();
+						displayCount++;
+					} else {
+						option.hide();
+					}
+				});
+
+				displayCount > 0 ? realmContainer.show() : realmContainer.hide();
+			}
+		} else {
+			hideRealmDropDown();
+		}
+	};
+
+	// Populate realm list.
+	api({ action: 'regions' }, function(res) {
+		if (!res.error) {
+			var regions = res.regions;
+			for (var regionID in regions) {
+				if (regions.hasOwnProperty(regionID)) {
+					var region = regions[regionID];
+					var container = $('<div/>').addClass('realm-container').appendTo(realmDropDown);
+					$('<div/>').addClass('realm-header').text(region.name).appendTo(container);
+
+					var realms = region.realms;
+					for (var realmSlug in realms) {
+						if (realms.hasOwnProperty(realmSlug))
+							$('<div/>')
+								.addClass('realm-option')
+								.text(realms[realmSlug])
+								.attr('data-region', regionID)
+								.attr('data-name', realms[realmSlug].toLowerCase() + ' (' + regionID + ')')
+								.attr('data-slug', realmSlug)
+								.appendTo(container);
+					}
+
+					realmContainers.push(container);
+				}
+			}
+		} else {
+			console.error('Encountered API error when retrieving realms: %o', res);
+		}
+
+		// Invoked once the realm data is obtained to validate any cached
+		// value the user's browser has added to the input field.
+		hideRealmDropDown();
+	});
+
+	// Listen for any clicks on .realm-option elements.
+	$(document).on('mouseenter click touchstart', '.realm-option', function() {
+		selectOption($(this));
+	});
+
 	// Setup attribute-driven external links.
 	$('.link').each(function() {
 		var target = $(this);
@@ -35,6 +165,40 @@ $(function() {
 					setTimeout(function() { next.select(); }, 0);
 				}
 			}
+		});
+	});
+
+	// Set-up realm selection list.
+	realmField.on('focus input', function() {
+		showRealmDropDown($(this).val());
+	}).on('blur', function() {
+		hideRealmDropDown();
+	});
+
+	// Register a click listener for the search button.
+	$('#button-search').on('click', function() {
+		// Ensure the user has selected a realm..
+		if (selectedRealm === null) {
+			setErrorStatus('Please select a valid realm first.');
+			return;
+		}
+
+		// Basic validation for the entered character name..
+		var characterName = characterField.val().trim().toLowerCase();
+		if (characterName.length === 0 || characterName.length > 12) {
+			setErrorStatus('Please enter a valid character name first.');
+			return;
+		}
+
+		setPendingStatus('Obtaining character profession data...');
+
+		api({
+			action: 'character',
+			region: selectedRealm.region,
+			realm: selectedRealm.realm,
+			character: characterName
+		}, function() {
+			hideStatus();
 		});
 	});
 
