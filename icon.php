d45a0b17e81499c406167062a487610bbdd3cd58
Kruithne <kruithne@gmail.com>
2018-06-08 21:16:35 +0100

    Add icon resource endpoint

---
+++ icon.php
@@ -0,0 +1,15 @@
+<?php
+	error_reporting(0);
+
+	require_once(__DIR__ . '/lib/api.php');
+
+	$api = new API();
+	header('Content-type: image/jpeg');
+
+	$iconName = $_GET['id'];
+	if (is_string($iconName))
+		$iconName = preg_replace('/[^a-z0-9_]/', '', strtolower($iconName));
+	else
+		$iconName = 'inv_misc_questionmark';
+
+	echo $api->getIconImage($iconName);
