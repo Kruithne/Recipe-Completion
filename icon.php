89b8aa982199ab1e73b775e1dc28ee661ca19d54
Kruithne <kruithne@gmail.com>
2018-06-09 00:38:18 +0100

    Return 304 from icon resource endpoint when cached

--- icon.php
+++ icon.php
@@ -3,10 +3,15 @@
 
 	require_once(__DIR__ . '/lib/api.php');
 
+	if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
+		header('HTTP/1.1 304 Not Modified');
+		die();
+	}
+
 	header('Pragma: public');
 	header('Cache-Control: max-age=86400');
 	header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + 86400));
-	header('Content-type: image/jpeg');
+	header('Content-Type: image/png');
 
 	$api = new API();
 
